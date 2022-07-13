7fc382a51ca1c3cab6c498696f1cebcb1c2dfa4a
ddophi98 <lightcloud98@gmail.com>
2022-07-13 22:27:13 +0900

    [add] #9 : 프로젝트 생성함

---
+++ GiveCake/GiveCake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiveCake
+//
+//  Created by 김동락 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
