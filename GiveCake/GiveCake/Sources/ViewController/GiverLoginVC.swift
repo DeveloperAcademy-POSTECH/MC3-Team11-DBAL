f5b708dc5af3c48048ccdf8bdbcc49fe8fabf0b4
ddophi98 <lightcloud98@gmail.com>
2022-07-14 16:37:12 +0900

    [add] #18 스토리보드 및 viewController 파일 추가

---
+++ GiveCake/GiveCake/Sources/ViewController/GiverLoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  GiverLoginVC.swift
+//  GiveCake
+//
+//  Created by 김동락 on 2022/07/14.
+//
+
+import UIKit
+
+class GiverLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- GiveCake/GiveCake/Sources/ViewController/GiverTabBarVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/GiverTabBarVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GiverTabBarVC.swift
 //  GiveCake
 //
 //  Created by 김동락 on 2022/07/14.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GiverTabBarVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
