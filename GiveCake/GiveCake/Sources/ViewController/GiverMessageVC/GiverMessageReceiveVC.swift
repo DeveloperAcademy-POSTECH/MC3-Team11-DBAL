0bfe6742dfdb8c5e5d106e627669ef8cd703bae4
Ground <27danny@naver.com>
2022-07-17 15:18:51 +0900

    [feat] #23 : message giver의 기본적인 ViewController 세팅 완료

---
+++ GiveCake/GiveCake/Sources/ViewController/GiverMessageVC/GiverMessageReceiveVC.swift
@@ -0,0 +1,29 @@
+//
+//  GiverMessageReceiveVC.swift
+//  GiveCake
+//
+//  Created by KimJS on 2022/07/17.
+//
+
+import UIKit
+
+class GiverMessageReceiveVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
