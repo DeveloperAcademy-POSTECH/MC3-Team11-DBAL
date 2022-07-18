45122c1b32be19fc79f865b2abf6e8500800b0a5
22Seongsoo <103010751+22Seongsoo@users.noreply.github.com>
2022-07-18 11:43:02 +0900

    [feat] #22 GiverLogin 구현
    뷰 만들기~

--- GiveCake/GiveCake/Sources/ViewController/GiverLoginVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/GiverLoginVC.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class GiverLoginVC: UIViewController {
-    
     // 스토리보드에서 버튼 객체 가져옴
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var toTabBarBtn: UIButton!
     
     override func viewDidLoad() {
@@ -17,15 +18,13 @@ class GiverLoginVC: UIViewController {
         // 버튼에 함수 연결
         toTabBarBtn.addTarget(self, action: #selector(onClickedToTabBarBtn(sender:)), for: .touchUpInside)
     }
-    
+  
     // tabbar 화면으로 이동
     @objc func onClickedToTabBarBtn(sender: UIButton) {
-        //        performSegue(withIdentifier: "loginToGiverTabBarSegue", sender: nil)
         let storyboard = UIStoryboard(name: "GiverTabBar", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "GiverTabBarVC") as! GiverTabBarVC
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true, completion: nil)
-        
     }
     
 }
