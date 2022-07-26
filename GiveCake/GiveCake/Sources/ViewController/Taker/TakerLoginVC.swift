31132e3545d8b7803cba6b5747c98a52c4277250
22Seongsoo <103010751+22Seongsoo@users.noreply.github.com>
2022-07-26 12:49:27 +0900

    Login UI 구현
    입장코드, Giver, Taker, Admin의 Login UI 구현했습니다

--- GiveCake/GiveCake/Sources/ViewController/Taker/TakerLoginVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/Taker/TakerLoginVC.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class TakerLoginVC: UIViewController {
+    // 스토리보드에서 버튼 객체 가져옴
+    @IBOutlet weak var toTabBarBtn: UIButton!
+    @IBOutlet weak var childNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 네비게이션바 숨기기
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        // 버튼에 함수 연결
+        toTabBarBtn.addTarget(self, action: #selector(onClickedToTabBarBtn(sender:)), for: .touchUpInside)
     }
+  
+    // tabbar 화면으로 이동
+    @objc func onClickedToTabBarBtn(sender: UIButton) {
+        performSegue(withIdentifier: "loginToTakerTabBarSegue", sender: nil)
+    }
+    
 }
