c22a4a552740766be812aae9848dc5d429a3880e
ddophi98 <lightcloud98@gmail.com>
2022-07-27 16:55:16 +0900

    [refactor] #70 코드 살짝 정리함

--- GiveCake/GiveCake/Sources/ViewController/Taker/TakerMessageVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/Taker/TakerMessageVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TakerMessageVC: UIViewController {
     fileprivate let messageList = TakerMessageModel.instance.messageList
-    fileprivate let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+    fileprivate let alert = UIAlertController(title: "편지가 전달 됐습니다.", message: "행복한 생일 보내세요~!", preferredStyle: .alert)
     fileprivate let blurView = UIVisualEffectView(effect: nil)
     
     @IBOutlet weak var tableView: UITableView!
@@ -20,23 +20,47 @@ class TakerMessageVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 테이블뷰 세팅하기
-        setTableView()
-        // 헤더의 그림자 세팅하기
-        setHeaderShadow()
+        // 테이블뷰 세팅
+        let myTableViewCellNib = UINib(nibName: "MessageCell", bundle: nil)
+        self.tableView.register(myTableViewCellNib, forCellReuseIdentifier: "MessageCell")
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.separatorStyle = .none
+    
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        // 헤더의 그림자 설정
+        header.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        header.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        header.layer.shadowOpacity = 1.0
+        header.layer.shadowRadius = 0.0
+        header.layer.masksToBounds = false
+        
         // 버튼 동작 연결
         writeBtn.addTarget(self, action: #selector(onClickedWriteBtn(sender:)), for: .touchUpInside)
-        // alert 세팅하기
-        setAlert()
-        // blurView 세팅하기
-        setBlurView()
+        
+        // Alert 설정하기
+        let okAction = UIAlertAction(title: "확인", style: .default) { _ in
+            // 확인 누르면 blur효과 지우기
+            UIView.animate(withDuration: 0.1, delay: 0, animations: {
+                self.blurView.alpha = 0.0
+            })
+        }
+        alert.addAction(okAction)
+        
+        // BlurView 설정
+        
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        screen.addSubview(blurView)
+        blurView.edges(to: screen)
+        blurView.alpha = 0.0
+        blurView.effect = UIBlurEffect(style: .light)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let vc = segue.destination as? TakerMessageWriteVC else {return}
-        // 메세지 작성 뷰에서 모달이 내려가면 실행할 코드
         vc.completionHandler = {
-            // 0.5초 있다가 alert 띄워주고 blur 효과주기
+            // 0.5초 있다가 alert 띄워주고 blur효과 주기
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 self.blurView.alpha = 0.9
                 self.present(self.alert, animated: true)
@@ -48,47 +72,8 @@ class TakerMessageVC: UIViewController {
     @objc fileprivate func onClickedWriteBtn(sender: UIButton) {
         performSegue(withIdentifier: "goToTakerMessageWrite", sender: nil)
     }
+
     
-    // tableView 세팅하기
-    fileprivate func setTableView() {
-        let myTableCellNib = UINib(nibName: "MessageCell", bundle: nil)
-        tableView.register(myTableCellNib, forCellReuseIdentifier: "MessageCell")
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.separatorStyle = .none
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-    
-    // 헤더의 그림자 세팅하기
-    fileprivate func setHeaderShadow() {
-        header.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
-        header.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        header.layer.shadowOpacity = 1.0
-        header.layer.shadowRadius = 0.0
-        header.layer.masksToBounds = false
-    }
-    
-    // alert 세팅하기
-    fileprivate func setAlert() {
-        alert.title = "편지가 전달 됐습니다."
-        alert.message = "행복한 생일 보내세요~!"
-        // 확인 누르면 blur효과 지우기
-        let okAction = UIAlertAction(title: "확인", style: .default) { _ in
-            UIView.animate(withDuration: 0.1, delay: 0, animations: {
-                self.blurView.alpha = 0.0
-            })
-        }
-        alert.addAction(okAction)
-    }
-    
-    // blurView 세팅하기
-    fileprivate func setBlurView() {
-        blurView.translatesAutoresizingMaskIntoConstraints = false
-        screen.addSubview(blurView)
-        blurView.edges(to: screen)
-        blurView.alpha = 0.0
-        blurView.effect = UIBlurEffect(style: .light)
-    }
 }
 
 extension TakerMessageVC: UITableViewDelegate, UITableViewDataSource {
