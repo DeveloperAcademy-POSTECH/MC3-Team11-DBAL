8749a011efe04ec9382b16667585b524cfb85540
HANS <103012796+HANS5109@users.noreply.github.com>
2022-07-28 11:12:04 +0900

    [feat] #77 GiverHomeView, 모인 케이크 개수에 따른 View전환 및 Label수정 기능

---
+++ GiveCake/GiveCake/Sources/ViewController/Admin/AdminHomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  AdminHomeVC.swift
+//  GiveCake
+//
+//  Created by YOON CHANG HYO on 2022/07/27.
+//
+
+import SwiftUI
+
+struct AdminHomeVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdminHomeVC_Previews: PreviewProvider {
+    static var previews: some View {
+        AdminHomeVC()
+    }
+}
