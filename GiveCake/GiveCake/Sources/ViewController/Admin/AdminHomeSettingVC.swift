1e2bc15556355e69f94e3db18df84ca18fb85850
HANS <103012796+HANS5109@users.noreply.github.com>
2022-07-30 17:26:15 +0900

    [feat] #99: UI View에 모서리값 부여함

--- GiveCake/GiveCake/Sources/ViewController/Admin/AdminHomeSettingVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/Admin/AdminHomeSettingVC.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class AdminHomeSettingVC: UIViewController{
+    @IBOutlet var controlBar: UIView!
+    @IBOutlet weak var currentCakeCountBox: UIView!
+    @IBOutlet weak var totalCakeCountBox: UIView!
+    @IBAction func confirmButton(_ sender: Any) {
+    }
+    @IBAction func currentCakeTextField(_ sender: Any) {
+    }
+    @IBAction func totalCakeTextField(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         
+        controlBar.layer.cornerRadius = 2
+        currentCakeCountBox.layer.cornerRadius = 15
+        totalCakeCountBox.layer.cornerRadius = 15
+
     }
     
 }
