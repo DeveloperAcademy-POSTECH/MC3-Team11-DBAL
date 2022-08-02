1ba40cbb484786ea1ec3a77a262caca9b6899a59
Ground <27danny@naver.com>
2022-08-03 00:15:08 +0900

    [feat] #109 : Giver Message에서 Taker들의 메시지 불러오기

--- GiveCake/GiveCake/Sources/ViewController/Giver/GiverMessageVC.swift
+++ GiveCake/GiveCake/Sources/ViewController/Giver/GiverMessageVC.swift
@@ -8,29 +8,36 @@
 import UIKit
 
 class GiverMessageVC: UIViewController {
-    fileprivate let messageList = MessageModel.instance.giverMessageList
+    //    fileprivate let messageList = MessageModel.instance.giverMessageList
+    fileprivate var messageList: [WrittenByTaker] = []
     let alert = UIAlertController(title: "마음이 전달 됐습니다.", message: "아이들이 행복한 세상! \n 늘 함께해주셔서 감사합니다~!", preferredStyle: .alert)
     
     @IBOutlet weak var writeBtn: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var header: UIView!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        let okAction = UIAlertAction(title: "확인", style: .default)
-        
-        alert.addAction(okAction)
-        
-        let myTableCellNib = UINib(nibName: "MessageCell", bundle: nil)
-        tableView.register(myTableCellNib, forCellReuseIdentifier: "MessageCell")
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.separatorStyle = .none
-        tableView.delegate = self
-        tableView.dataSource = self
-        setHeaderShadow()
+        FirebaseManager.instance.loadData(collectionName: "WrittenByTaker") { isSuccess, conveyedArray in
+            if isSuccess {
+                self.messageList = conveyedArray as? [WrittenByTaker] ?? []
+                
+                let okAction = UIAlertAction(title: "확인", style: .default)
+                
+                self.alert.addAction(okAction)
+                
+                let myTableCellNib = UINib(nibName: "MessageCell", bundle: nil)
+                self.tableView.register(myTableCellNib, forCellReuseIdentifier: "MessageCell")
+                self.tableView.rowHeight = UITableView.automaticDimension
+                self.tableView.separatorStyle = .none
+                self.tableView.delegate = self
+                self.tableView.dataSource = self
+                self.setHeaderShadow()
+            } else {
+                print("통신에 어떤 식으로든 문제가 발생했다...")
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,8 +56,8 @@ class GiverMessageVC: UIViewController {
         header.layer.shadowRadius = 2.0
         header.layer.masksToBounds = false
     }
-
-
+    
+    
 }
 
 
@@ -60,9 +67,11 @@ extension GiverMessageVC: UITableViewDelegate, UITableViewDataSource{
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath) as! MessageCell
-        cell.emoticonLabel.text = messageList[indexPath.row].emoticon
-        cell.senderLabel.text = messageList[indexPath.row].sender
-        cell.contentLabel.text = messageList[indexPath.row].content
+        //        cell.emoticonLabel.text = messageList[indexPath.row].emoticon
+        //        cell.emoticonLabel.text = MessageModel.emoticons[indexPath.row]
+        cell.emoticonLabel.text = MessageModel.emoticons[indexPath.row]
+        cell.senderLabel.text = messageList[indexPath.row].takerName
+        cell.contentLabel.text = messageList[indexPath.row].messageFromTakerToGiver
         
         return cell
     }
